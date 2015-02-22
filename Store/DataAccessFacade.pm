a03da31fc26cbd3a2d4cbc3d19c205167efbbdf4
Justin Yang <linuxjustin@gmail.com>
2015-02-23 00:37:07 +0800

    Add several data access methods.

--- Store/DataAccessFacade.pm
+++ Store/DataAccessFacade.pm
@@ -4,26 +4,99 @@ use strict;
 use warnings;
 use 5.010;
 
-use Store::RedisStoreProvider qw(get_connection);
+use Store::RedisStoreProvider qw(get_connection commit_transaction);
 
 use Exporter qw(import);
 
 use parent 'Base';
 
-sub update_channel {
+sub add_channel {
     my $this = shift;
 
     my $chanid = shift;
     my $uaid = shift;
-    my $version = shift;
     my $conn = get_connection();
+    my $action = sub {
+                        $conn->hset("chanid:$chanid", 'uaid' => $uaid);
+                        $conn->sadd("uaid:$uaid", $chanid);
+                 };
     
-    $conn->hmset("chanid:$chanid", 'uaid' => $uaid, 'version' => $version);
+    commit_transaction($action, $conn);
 }
 
-sub update_chanid_list_for_uaid {
+sub remove_channel {
     my $this = shift;
+
+    my $chanid = shift;
+    my $uaid = shift;
+    my $conn = get_connection();
+    my $action = sub {
+                        $conn->del("chanid:$chanid");
+                        $conn->srem("uaid:$uaid", $chanid);
+                 };
     
+    commit_transaction($action, $conn);
+}
+
+sub update_channel_version {
+    my $this = shift;
+
+    my $chanid = shift;
+    my $version = shift;
+    my $conn = get_connection();
+
+    $conn->hset("chanid:$chanid", 'version' => $version);
+}
+
+sub remove_user_agent {
+    my $this = shift;
+
+    my $uaid = shift;
+    my $conn = get_connection();
+    my @chanids = $conn->smembers("uaid:$uaid");
+    my $action = sub {
+                        foreach my $chanid (@chanids) {
+                            $conn->del("chanid:$chanid");
+                        }
+                        $conn->del("uaid:$uaid");
+                 };
+    
+    commit_transaction($action, $conn);
+}
+
+sub is_user_agent_exists {
+    my $this = shift;
+
+    my $uaid = shift;
+    my $conn = get_connection();
+    
+    return $conn->exists("uaid:$uaid");
+}
+
+sub get_user_agent_by_channel {
+    my $this = shift;
+
+    my $chanid = shift;
+    my $conn = get_connection();
+
+    return _get_attribute_by_channel($chanid, 'uaid');
+}
+
+sub get_channel_version {
+    my $this = shift;
+
+    my $chanid = shift;
+    my $conn = get_connection();
+
+    return _get_attribute_by_channel($chanid, 'version');
+}
+
+sub _get_attribute_by_channel {
+    my $chanid = shift;
+    my $attr = shift;
+    my $conn = get_connection();
+
+    return $conn->hget("chanid:$chanid", $attr);
 }
 
 1;
--- Store/RedisStoreProvider.pm
+++ Store/RedisStoreProvider.pm
@@ -10,6 +10,7 @@ use Exporter qw(import);
 
 our @EXPORT_OK = qw/
                   get_connection  
+                  commit_transaction
                   /;
 
 sub get_connection {
@@ -17,6 +18,15 @@ sub get_connection {
     my $redis = defined $config ? Redis->new($config) : Redis->new;
 
     return $redis;
+}
+
+sub commit_transaction {
+    my $action = shift;
+    my $conn = shift;
+
+    $conn->multi;
+    $action->();
+    $conn->exec;
 }
 
 1;
